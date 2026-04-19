0742193c0505238ea355618c8a57baa4a0e95873
Akshat Karn <128296751+dev0929-ak@users.noreply.github.com>
2026-04-19 18:26:47 +0530

    Modify Virus.bat with comments and path instructions
    Updated Virus.bat to include comments about the virus.vbs file location and recommendations for users.

--- Virus.bat
+++ Virus.bat
@@ -3,4 +3,6 @@ color 0a
 echo CONGRATS! YOU OPENED THE COMPUTER, NOW YOUR PC WILL BE RUINED HAHAHAHAHA!!!!!
 shutdown -s -t 30 -c "Your Computer is cooked, now enjoy playing with this little messagebox in 10 secondswhile your computer will be shutdown"
 timeout /t 10
-wscript //nologo "C:\Users\aksha\OneDrive\My Documents\My Other files\virus.vbs"
+REM You need to download the virus.vbs folder which is uploaded in My VBS Files repository as this is also needed and also in the below line you need to put your folder path where you saved this virus.vbs folder
+REM I would also recommend to delete the REM line after downloading the program, although it's just a comment line but still
+wscript //nologo "--Directory of where you saved the virus.vbs folder--"
